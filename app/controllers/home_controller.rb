4103b42626a34d6c580a11d8cab57702fdfc255e
manh <n-manh@ruby-dev.jp>
2019-04-02 18:32:16 +0700

    ticket9619 : Create rubocop settings and fix code for v2

--- app/controllers/home_controller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# class StaticPagesController
-class StaticPagesController < ActionController::Base
-  def home; end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2019_04_02_061112) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
