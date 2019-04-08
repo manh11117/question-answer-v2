b4536221d2e6f09806399f2dc468cecc76c27572
manh <n-manh@ruby-dev.jp>
2019-04-08 16:51:29 +0700

    ticket9698 : Create form for create/update users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,8 +4,43 @@
 module Admin
   # Controller for Users
   class UsersController < ApplicationController
+    before_action :find_user, only: %i[edit update]
+
     def index
       @users = User.order(:id).page(params[:page]).per(10)
+    end
+
+    def new
+      @user = User.new
+    end
+
+    def edit; end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to admin_users_path
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @user.update(user_params)
+        redirect_to admin_users_path
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
+    end
+
+    def find_user
+      @user = User.find(params[:id])
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   enum role: { admin: 0, user: 1 }
   validates :name, presence: true, length: { maximum: 50 }
-  validates :password, presence: true, length: { minimum: 6 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :role, presence: true
 end
