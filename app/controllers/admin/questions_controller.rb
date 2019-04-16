0cf1c21684955b2ede38f9d14e8decbc7e44a265
manh <n-manh@ruby-dev.jp>
2019-04-16 09:03:12 +0700

    save

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -3,10 +3,6 @@
 module Admin
   # class QuestionsController
   class QuestionsController < ApplicationController
-<<<<<<< HEAD
-=======
-    # http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
->>>>>>> 5c0975a... ticket9794 : Design and create questions and answers tables and models
     def index
       @questions = Question.all
     end
