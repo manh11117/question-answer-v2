da4d4b98653214de4faaedf3f4b0bbf400f2d9a1
manh <n-manh@ruby-dev.jp>
2019-04-16 09:03:12 +0700

    ticket9794 : Design and create questions and answers tables and models

---
+++ db/migrate/20190416020138_create_answers.rb
@@ -0,0 +1,11 @@
+class CreateAnswers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :answers do |t|
+      t.text :text
+      t.boolean :flag, default: false
+      t.references :question, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,31 @@ User.create!(id:  1, name:  "admin", email: "admin@gmail.com", password: "passwo
 49.times do |n|
   User.create!(id:  n+2, name:  "user#{n+1}", email: "user#{n+1}@gmail.com", password: "password", role: [0, 1].sample)
 end
+
+Question.destroy_all
+Answer.destroy_all
+q = Question.create(id:  1, text: "Which one is not an object oriented programming language?")
+q.answers.create(id:  1, question_id: q.id, text: "Java", flag: false)
+q.answers.create(id:  2, question_id: q.id, text: "C#", flag: false)
+q.answers.create(id:  3, question_id: q.id, text: "C++", flag: false)
+q.answers.create(id:  4, question_id: q.id, text: "C", flag: true)
+q = Question.create(id:  2, text: "Which language is used for styling web pages?")
+q.answers.create(id:  5, question_id: q.id, text: "HTML", flag: false)
+q.answers.create(id:  6, question_id: q.id, text: "JQuery", flag: false)
+q.answers.create(id:  7, question_id: q.id, text: "CSS", flag: true)
+q.answers.create(id:  8, question_id: q.id, text: "XML", flag: false)
+q = Question.create(id:  3, text: "There are ____ main components of object oriented programming.")
+q.answers.create(id:  9, question_id: q.id, text: "1", flag: false)
+q.answers.create(id:  10, question_id: q.id, text: "6", flag: false)
+q.answers.create(id:  11, question_id: q.id, text: "2", flag: false)
+q.answers.create(id:  12, question_id: q.id, text: "4", flag: true)
+q = Question.create(id:  4, text: "Which language is used for web apps?")
+q.answers.create(id:  13, question_id: q.id, text: "PHP", flag: false)
+q.answers.create(id:  14, question_id: q.id, text: "Python", flag: false)
+q.answers.create(id:  15, question_id: q.id, text: "Javascript", flag: false)
+q.answers.create(id:  16, question_id: q.id, text: "All", flag: true)
+q = Question.create(id:  5, text: "MVC is a ____.")
+q.answers.create(id:  17, question_id: q.id, text: "Language", flag: false)
+q.answers.create(id:  18, question_id: q.id, text: "Library", flag: false)
+q.answers.create(id:  19, question_id: q.id, text: "Framework", flag: true)
+q.answers.create(id:  20, question_id: q.id, text: "All", flag: false)
